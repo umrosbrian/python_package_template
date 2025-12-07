1e7e8fe0a027454a79a438b808ae80b78a1865a3
Brian Ross <rosbrian@umich.edu>
2025-12-07 08:39:51 -0500

    added custom CSS

--- docs/conf.py
+++ docs/conf.py
@@ -94,5 +94,5 @@ templates_path = ['_templates']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_theme = "sphinx_rtd_theme"
-html_css_files = ["readthedocs-custom.css"] # Override some CSS settings
+html_css_files = ["custom.css", "readthedocs-custom.css"] # Override some CSS settings
 html_static_path = ['_static']
